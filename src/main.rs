4e2eab70a14150103433813558d5c8f8e6c41f01
biscuitrescue <karttikeyasinha11@gmail.com>
2025-09-27 17:40:31 +0530

    print to vgabug using write

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static MSG: &[u8] = b"CrabOS Supremacy";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::printsmth();
 
-    for (i, &byte) in MSG.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -48,5 +48,51 @@ struct Buffer {
 pub struct Writer {
     column_position: usize,
     color_code: ColorCode,
-    buffer: &'static mut Buffer,
+    buffer: &'static mut Buffer, // lifetime valid for whole prog run time -> VGA Buff
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH { self.new_line(); }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    // Since rust strings are utf-8
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII || newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not printable ASCII
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn new_line(&mut self) {} // TODO
+}
+
+pub fn printsmth() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Green, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'C');
+    writer.write_string("rabOS");
+    writer.write_string("Supremacy");
 }
