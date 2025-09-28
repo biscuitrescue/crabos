149e4ec6fab9926fa890b8194a095e9178a34543
biscuitrescue <karttikeyasinha11@gmail.com>
2025-09-29 02:34:31 +0530

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
@@ -16,5 +18,14 @@ pub extern "C" fn _start() -> ! {
     println!("CrabOS Supremacy{}", "!");
     loop {
         panic!("Panic msg");
+    }
+}
+
+// tests
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
     }
 }
